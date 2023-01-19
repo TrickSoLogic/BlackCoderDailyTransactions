278104f9e5ee0a0a8190a93b32d7fba446b0b358
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-19 23:36:00 +0530

    Add project files.

---
+++ BookListRazor/Migrations/20230115173545_init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlackCoderDailyTransactions.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyTransaction",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TransactionName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransactionDetails = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransactionInAmt = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TransactionOutAmt = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TransactionBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyTransaction", x => x.TransactionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyTransaction");
+        }
+    }
+}
---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackCoderDailyTransactions.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext( DbContextOptions <ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DailyTransaction> DailyTransaction { get; set; }
+
+
+
+    }
+
+}
